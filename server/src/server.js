4a2adb7fa6c4d2bd624dc472808b4123d2ef6819
CGAJAY <jmugendi737@gmail.com>
2024-10-10 12:23:05 +0300

    added middleware to parse json and added endpoints for routers

--- server/src/server.js
+++ server/src/server.js
@@ -1,19 +1,22 @@
 import express from "express";
 import { configDotenv } from "dotenv";
 import connectDb from "./database/connectDb.js";
+import authRouter from "./routes/authRoutes.js";
+import balanceRouter from "./routes/balanceRoutes.js";
+import transactionsRouter from "./routes/transactionRoutes.js";
 
 configDotenv(); // Load environment variables
 
 const PORT = process.env.PORT;
 
+// Create an Express application
 const app = express();
+app.use(express.json()); // Middleware to parse JSON bodies
 connectDb();
 
-app.get("/", (req, res) => {
-	res.json({
-		message: "PhP dev are not gen z's",
-	});
-});
+app.use("/auth", authRouter);
+app.use("/balance", balanceRouter);
+app.use("/transactions", transactionsRouter);
 
 app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`);
