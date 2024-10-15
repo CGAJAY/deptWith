51d77e6b3cdf26c8c6e0f26eed549d411739725b
CGAJAY <jmugendi737@gmail.com>
2024-10-15 20:11:42 +0300

    Updated the register user function to validate name and email

--- server/src/server.js
+++ server/src/server.js
@@ -1,33 +1,22 @@
 import express from "express";
 import { configDotenv } from "dotenv";
 import connectDb from "./database/connectDb.js";
-import authRouter from "./routes/authRoutes.js";
-import balanceRouter from "./routes/balanceRoutes.js";
-import transactionsRouter from "./routes/transactionRoutes.js";
-
 import { v1Router } from "./routes/v1/index.js";
 import { v2Router } from "./routes/v2/index.js";
 
 configDotenv(); // Load environment variables
 
+connectDb();
+
 const PORT = process.env.PORT;
 
 // Create an Express application
 const app = express();
-app.use(express.json()); // Middleware to parse JSON bodies
-connectDb();
 
-app.use("/auth", authRouter);
-app.use("/balance", balanceRouter);
-app.use("/transactions", transactionsRouter);
+// Middleware to parse JSON bodies
+app.use(express.json());
 
 // ROUTES
-app.get("/", (req, res) => {
-	console.log("Request received on root path");
-	res.json({
-		message: "Silence is golden",
-	});
-});
 
 // END ROUTES
 app.use("/api/v1", v1Router);
