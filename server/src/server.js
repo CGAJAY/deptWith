47815a3191432654b48a4ab44f314e0a987291d1
CGAJAY <jmugendi737@gmail.com>
2024-10-15 13:07:25 +0300

    Added the routes in my server

--- server/src/server.js
+++ server/src/server.js
@@ -5,6 +5,9 @@ import authRouter from "./routes/authRoutes.js";
 import balanceRouter from "./routes/balanceRoutes.js";
 import transactionsRouter from "./routes/transactionRoutes.js";
 
+import { v1Router } from "./routes/v1/index.js";
+import { v2Router } from "./routes/v2/index.js";
+
 configDotenv(); // Load environment variables
 
 const PORT = process.env.PORT;
@@ -17,6 +20,25 @@ connectDb();
 app.use("/auth", authRouter);
 app.use("/balance", balanceRouter);
 app.use("/transactions", transactionsRouter);
+
+// ROUTES
+app.get("/", (req, res) => {
+	console.log("Request received on root path");
+	res.json({
+		message: "Silence is golden",
+	});
+});
+
+// END ROUTES
+app.use("/api/v1", v1Router);
+
+app.use("/api/v2", v2Router);
+
+app.use("*", (req, res) => {
+	res.status(404).json({
+		message: "Not found",
+	});
+});
 
 app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`);
