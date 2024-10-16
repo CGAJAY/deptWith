4585a078a667fac061d39ff855d8b0f6f00d6ca3
CGAJAY <jmugendi737@gmail.com>
2024-10-16 19:43:08 +0300

    Added bycrypt package and added middleware to registerUser endpoint

--- server/src/routes/v1/auth.js
+++ server/src/routes/v1/auth.js
@@ -9,17 +9,27 @@ import {
 	registerUser, // Function for handling new user registration
 } from "../../controllers/auth.js"; // Importing from the controllers/auth.js file
 
+// Importing the validation middleware functions for login and registration
+import {
+	validateUserLogin,
+	validateUserRegistration,
+} from "../../middleware/validators.js";
+
 // Create an instance of the Express Router
 // authRouter will handle routes related to authentication (e.g., login and registration)
 const authRouter = Router();
 
 // Define a POST route for user registration
 // This route is used for registering a new user
-authRouter.post("/register", registerUser);
+authRouter.post(
+	"/register",
+	validateUserRegistration,
+	registerUser
+);
 
 // Define a POST route for user login
 // This route is used for logging in an existing user
-authRouter.post("/login", loginUser);
+authRouter.post("/login", validateUserLogin, loginUser);
 
 // Export the authRouter so it can be used in other parts of the application
 export { authRouter };
