609970ba473a5cc59267b8600ced5659339e2a80
CGAJAY <jmugendi737@gmail.com>
2024-10-23 21:08:32 +0300

    generated a token & store token in a cookie

---
+++ server/src/utils/generate-jwt-token.js
@@ -0,0 +1,20 @@
+import jwt from "jsonwebtoken";
+
+// Generate a JWT token using the 'user' object as payload
+export const generateJwtToken = (res, user) => {
+	// payload includes the user details like the user's _id
+	// jwt.sign() takes in the payload, secret key and options such as the token's expiry.
+
+	const token = jwt.sign({ user }, process.env.JWT_SECRET, {
+		expiresIn: "1d", // Token set to expire in 1 day
+	});
+
+	// set the token in a cookie in the response 'res' object
+	// Stores the JWT in a cookie for secure client-side storage
+	res.cookie(process.env.AUTH_COOKIE_NAME, token, {
+		httpOnly: true, // Prevents JS from accessing the cookie(protection against XSS attacks)
+		maxAge: 24 * 60 * 60 * 1000, //Cookie expires in 1 day
+		sameSite: "strict", // Ensures the cookie is only sent in requests originating from the same site
+		secure: process.env.NODE_ENV === "production", // When in production mode, cookie is sent over HTTPS
+	});
+};
