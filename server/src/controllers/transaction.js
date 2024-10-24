c8ab6202fd22f3704b66f2726862236dc2b30ceb
CGAJAY <jmugendi737@gmail.com>
2024-10-24 20:26:32 +0300

    Validate amount & changed transaction format

--- server/src/controllers/transaction.js
+++ server/src/controllers/transaction.js
@@ -1,3 +1,39 @@
+import { isValidObjectId } from "mongoose";
+import { Balance } from "../database/models/Balance.model.js";
+import { User } from "../database/models/User.model.js";
+import { Transaction } from "../database/models/Transaction.model.js";
+import { StatusCodes } from "http-status-codes";
+
+export const createTransaction = async (req, res) => {
+	// Get id of currently logged in user from request object
+	const { userId } = req;
+
+	// get transaction details from the request body
+	let { amount, transactionType } = req.body;
+
+	// Check if type of amount is not a number
+	// isNaN checks if result is (NaN), if true code executes
+	if (isNaN(parseInt(amount))) {
+		return res
+			.status(StatusCodes.BAD_REQUEST)
+			.json({ message: "Amount must be a number" });
+	}
+
+	// Check if amount is === 0
+	if (parseInt(amount) === 0) {
+		return res
+			.status(StatusCodes.BAD_REQUEST)
+			.json({ message: "Amount must be more than 0" });
+	}
+
+	// Grab transactionType and reasign it the value in the correct format
+	// transactionType.charAt(0).toUpperCase(); get 1st char & converts to upperCase
+	// transactionType.slice(1).toLowerCase(); get rest of the string and converts to lowerCase
+	transactionType =
+		transactionType.charAt(0).toUpperCase() +
+		transactionType.slice(1).toLowerCase();
+};
+
 // Check if user is logged in
 // Get the balance of the logged-in user
 // Validate the transaction by checking if  (amount > 0 && amount < balance)
@@ -5,10 +41,6 @@
 // Grab the recepient's balance
 // Deduct amount from the sender's balance
 // Add amount to recipients's balance
-
-import { isValidObjectId } from "mongoose";
-import { Balance } from "../database/models/Balance.model.js";
-import { User } from "../database/models/User.model.js";
 
 export const sendTransaction = async (req, res) => {
 	const { amount, recipientId } = req.body;
